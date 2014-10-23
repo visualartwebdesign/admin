f077e4df55719c3c854ace0f9929aff44bd96c79
nico <nicotaglienti@gmail.com>
2014-10-23 18:22:36 -0300

    no message

--- configBD.php
+++ configBD.php
@@ -14,59 +14,7 @@
 				1 si es obligatorio - 0 no
 
 3=> Cuadro de 	
-<<<<<<< HEAD
-asdasdasdasd
-asda
 
 	seleccion
 
-			
-
-fjdsñlkfjasd
- fdskaf jas
- d
-
-function nicote (){
 	
-'hola como va?'
-
-
-}
-kd sjfa
-asdjfkasdj fasd f 
-dsa
-sadf
-sd
- fdsa
- df
- a
- ad
- sfas
- 
- 
- function bater(){
- 	sos un crack;
- 	igual que vos master;
- 	ni ahi vos sos mas;cno seas humilde vos sos mas;
- }
-
-<<<<<<< HEAD
-fdkasjfñlaksd 
-=======
- function baterflowman(){
- 	sos un crack;
- 	igual que vos master;
- 	ni ahi vos sos mas;cno seas humilde vos sos mas;
- }
-
-=======
->>>>>>> 891c7fcb6ab959eca60e805329da40f8b5d4aae8
-
-
-	seleccion
-
-<<<<<<< HEAD
-estoy es lo de nicote
->>>>>>> 775c11e337cc16bb0f8908cc51d904bd507bc3c9
-=======
->>>>>>> 891c7fcb6ab959eca60e805329da40f8b5d4aae8
