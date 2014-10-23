5fdfe1a0134975e33dc72abdd37fe3069143cfbd
Cristian <cmbadaracco@gmail.com>
2014-10-22 22:24:20 -0300

    agregar configuracion BD

---
+++ configBD.php
@@ -0,0 +1,18 @@
+############ Configuracion Base de Datos ###########
+
+
+	INPUT		PARAMETROS
+1 => text		1
+				Nombre del label
+				Cantidad de valores
+				1 si es obligatorio - 0 no
+
+
+2=> number		2
+				Nombre
+				Cantidad
+				1 si es obligatorio - 0 no
+
+3=> Cuadro de 	
+	seleccion
+			
